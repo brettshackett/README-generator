09ba8523709ce5557606ffc0881133e76ba4dafe
brettshackett <63305041+brettshackett@users.noreply.github.com>
2022-04-24 22:52:41 -0400

    added the prompt and got the file to read the inquirer package

---
+++ Develop/README.md
@@ -0,0 +1,28 @@
+# README Generator
+    
+#### Table of Contents
+1. [Project Description](#project-description)
+2. [Installation Instructions](#installation-instructions)
+3. [Usage Information](#usage-information)
+4. [Contributor Guidelines](#contributor-guidelines)
+5. [Code of Conduct](#code-of-conduct)
+6. [Test Instructions](#test-instructions)
+7. [License](#license)
+8. [Questions](#questions)
+## Project Description
+* This project allows users to quickly generate a README file that includes the answers that were input, this helps users save time and begin coding sooner and spend less time putting together a good README file
+## Installation Instructions
+* yes
+## Usage Information
+* Quickly generate a thorough README
+## Contributor Guidelines
+* no
+## Code of Conduct
+* [Contributor Covenant Code of Conduct](https://www.contributor-covenant.org/version/2/0/code_of_conduct/code_of_conduct.md)
+## Test Instructions
+* none
+## License
+* licensed under the 
+## Questions
+* For additional help or questions about collaboration, please reach out to shackettbrett@gmail.com
+* Follow me on Github at [brettshackett](http://github.com/brettshackett)
--- Develop/index.js
+++ Develop/index.js
@@ -1,156 +1,107 @@
+const inquirer = require("inquirer")
+
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown.js');
+const fs = require("fs");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
 
-// TODO: Create an array of questions for user input
-const questions = [
-    {
-        //project title
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of the project?',
-        validate: titleInput => {
-            if (titleInput){
-                return true;
-            }else{
-                console.log('You need to enter a title, try again!');
-                return false;
-            }
-        }
-    },
+function promptUser() {
     
-    // project description
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Please provide a description of your project.',
-        validate: descriptionInput => {
-            if(descriptionInput){
-                return true;
-            }else{
-                console.log('You need to provide a description of your project, try again!');
-                return false
-            }
-        }
-    },
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "project_title",
+            message: "What is your project tittled?"
+          },
+          {
+            type: "input",
+            name: "description",
+            message: "Briefly describe your project"
+          },
+          {
+            type: "input",
+            name: "install",
+            message: "Are there any installations required?"
+          },
+          {
+            type: "input",
+            name: "use",
+            message: "What is the use of the application"
+          },
+          {
+            type: "input",
+            name: "contributions",
+            message: "Are there any contribution rules?"
+          },
+          {
+            type: "input",
+            name: "test",
+            message: "Please provide test instructions if applicable"
+          },
+          {
+            type: "checkbox",
+            message: "License?",
+            name: "license",
+            choices: [
+              "[MIT License](LICENSE.txt)", 
+              "[GNU GPLv3 License](COPYING.txt)", 
+            ]
+          },
+          {
+            type: "input",
+            name: "email",
+            message: "Enter your email account"
+          },
+          {
+            type: "input",
+            name: "github",
+            message: "Enter your github username"
+          }
+        ]);
+      }
 
-    // install instructions
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'How do you install your project?',
-        validate: installInput => {
-            if(installInput){
-                return true;
-            }else{
-                console.log('You need to provide information about installing your project, try again!');
-                return false;
-            }
-        }
-    },
-
-    //usage information
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'How do you use this project?',
-        validate: usageInput => {
-            if(usageInput){
-                return true;
-            }else{
-                console.log('You need to provide information about using your project, try again!');
-                return false;
-            }
-        }
-    },
-
-       // contribution guidelines
-       {
-        type: 'input',
-        name: 'contribution',
-        message: 'How should people contribute to this project? (Required)',
-        validate: contributionInput => {
-            if (contributionInput) {
-                return true;
-            } else {
-                console.log('You need to provide information on how to contribute to the project!');
-                return false;
-            }
-        }
-    },
-
-    // test instructions
-    {
-        type: 'input',
-        name: 'testing',
-        message: 'How do you test this project? (Required)',
-        validate: testingInput => {
-            if (testingInput) {
-                return true;
-            } else {
-                console.log('You need to describe how to test this project!');
-                return false;
-            }
-        }
-    },
-
-    // license options
-    {
-        type: 'checkbox',
-        name: 'license',
-        message: 'Choose a license for your project (Required)',
-        choices: ['Apache', 'MIT', 'Mozilla-Public', 'GNU-General-Public', 'Common-Development-and Distribution', 'None'],
-        validate: licensingInput => {
-            if (licensingInput) {
-                return true;
-            } else {
-                console.log('You must pick a license for the project!');
-                return false;
-            }
-        }
-    },
-
-    // github username
-    {
-        type: 'input',
-        name: 'github',
-        message: 'Enter your GitHub Username (Required)',
-        validate: githubInput => {
-            if (githubInput) {
-                return true;
-            } else {
-                console.log('Please enter your GitHub username!');
-                return false;
-            }
-        }
-    },
-
-    // email address
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Would you like to include your email?',
-    },
-];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) => {
-        if (err)
-            throw err;
-        console.log('Success! Information transferred to the README!')
-    });
+function generateREADME(answers) {
+  return `# ${answers.project_title}
+    
+#### Table of Contents
+1. [Project Description](#project-description)
+2. [Installation Instructions](#installation-instructions)
+3. [Usage Information](#usage-information)
+4. [Contributor Guidelines](#contributor-guidelines)
+5. [Code of Conduct](#code-of-conduct)
+6. [Test Instructions](#test-instructions)
+7. [License](#license)
+8. [Questions](#questions)
+## Project Description
+* ${answers.description}
+## Installation Instructions
+* ${answers.install}
+## Usage Information
+* ${answers.use}
+## Contributor Guidelines
+* ${answers.contributions}
+## Code of Conduct
+* [Contributor Covenant Code of Conduct](https://www.contributor-covenant.org/version/2/0/code_of_conduct/code_of_conduct.md)
+## Test Instructions
+* ${answers.test}
+## License
+* licensed under the ${answers.license}
+## Questions
+* For additional help or questions about collaboration, please reach out to ${answers.email}
+* Follow me on Github at [${answers.github}](http://github.com/${answers.github})`;
+  
 }
 
-// TODO: Create a function to initialize app
-function init() {
-    inquirer.prompt(questions)
-    .then(function (userInput) {
-        console.log(userInput)
-        writeToFile("README.md", generateMarkdown(userInput));
-    });
-}
+promptUser()
+  .then(function(answers) {
+    const readme = generateREADME(answers);
 
-// Function call to initialize app
-init();
+ 
+    return writeFileAsync("README.md", readme);
+  })
+  .then(function() {
+    console.log(" README.md has been created!");
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
